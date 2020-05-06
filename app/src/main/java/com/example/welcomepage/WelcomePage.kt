027f1395ef60fde83af3e90561cea3f089d57ac5
Dragino <dracossos@gmail.com>
2020-05-06 02:55:19 +0100

    initial of project

---
+++ app/src/main/java/com/example/welcomepage/WelcomePage.kt
@@ -0,0 +1,88 @@
+package com.example.welcomepage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_welcome_page.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+
+class WelcomePage : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var homeFragment: HomeFragment
+    lateinit var dishesFragment: DishesFragment
+    lateinit var settingsFragment: SettingsFragment
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_page)
+
+        setSupportActionBar(toolBar)
+        val actionBar = supportActionBar
+        actionBar?.title = "Navigation Drawer"
+
+        val drawerToggle: ActionBarDrawerToggle = object: ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolBar,
+            (R.string.open),
+            (R.string.close)
+        ){
+        }
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+        homeFragment = HomeFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_layout, homeFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.home -> {
+                homeFragment = HomeFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, homeFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+            R.id.dishes -> {
+                dishesFragment = DishesFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, dishesFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+            R.id.settings -> {
+                settingsFragment = SettingsFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout, settingsFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
